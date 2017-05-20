cea2849a53a7e62ed460602cd408de0d86a34325
michael <michael.malevannyy@gmail.com>
2017-05-20 22:53:17 +0300

    +++

---
+++ src/enterprise/datamapper/Person.java
@@ -0,0 +1,35 @@
+package enterprise.datamapper;
+
+
+// domain object POJO
+public class Person {
+    private int idperson;
+    private String firstName;
+    private String lastName;
+
+    public int getIdperson() {
+        return idperson;
+    }
+
+    public void setIdperson(int idperson) {
+        this.idperson = idperson;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
